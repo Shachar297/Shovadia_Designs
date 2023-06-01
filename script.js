7fa935dd27fa68370ee7644272960da48d4889ba
Shachar297 <thisishachar@gmail.com>
2023-06-01 15:47:10 +0300

    commit

--- script.js
+++ script.js
@@ -12,11 +12,9 @@ function setListeners() {
             e = e.target;
             console.log(e)
             e.style.transform = `scale(1.8, 1.8)`;
-            e.style.border = `solid rgba(255, 217, 0, 0.409) 1vh`
 
             setTimeout(() => {
                 e.style.transform = `scale(1, 1)`;
-                e.style.border = `none`;
 
 
             }, 800)
@@ -27,11 +25,9 @@ function setListeners() {
             e = e.target;
             console.log(e)
             e.style.transform = `scale(2, 2)`;
-            e.style.border = `solid rgba(255, 217, 0, 0.409) 1vh`
 
             setTimeout(() => {
                 e.style.transform = `scale(1, 1)`;
-                e.style.border = `none`;
             }, 2000)
         })
     })
